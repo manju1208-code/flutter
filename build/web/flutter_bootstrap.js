3c7c0cd56eb243a561cb3ef580c4a9d24d82cbc5
Manjula <meegmail.com>
2026-06-03 15:53:52 +0530

    Todo app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b05a9d7488655f18504327080d697d0a80c7c6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "968370127" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "21029964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
